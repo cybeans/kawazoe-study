5adb8cfe73b2f82b675ec40c17813ca4fd092eea
yoshioka <yoshioka@cybeans.co.jp>
2014-12-07 17:56:00 +0900

    初Commit！

--- kawazoe/src/HelloWorld.java
+++ kawazoe/src/HelloWorld.java
@@ -1,11 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 public class HelloWorld extends HttpServlet {
 
   public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -15,7 +10,7 @@ public class HelloWorld extends HttpServlet {
     PrintWriter out = response.getWriter();
     out.println("<html>");
     out.println("<head>");
-    out.println("<title>Hello World!</title>");
+    out.println("<title>Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</title>");
     out.println("</head>");
     out.println("<body>");
     out.println("<h1>Hello World!</h1>");
